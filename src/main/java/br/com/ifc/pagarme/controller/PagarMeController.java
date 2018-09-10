2aad4dacaffb7a2cb7497b7e2c96b9bd0ee0f0f0
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-10 15:57:50 -0300

    -- Teste

--- src/main/java/br/com/ifc/pagarme/controller/PagarMeController.java
+++ src/main/java/br/com/ifc/pagarme/controller/PagarMeController.java
@@ -4,6 +4,7 @@ import br.com.ifc.pagarme.model.Resposta;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pagarme")
+@RefreshScope
 public class PagarMeController {
 	
 	@Autowired

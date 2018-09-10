f036b8c67197bc4529e3e3b657144a517facb9b7
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-10 14:53:34 -0300

    - Primeira versão

---
+++ src/main/java/br/com/ifc/pagarme/controller/PagarMeController.java
@@ -0,0 +1,33 @@
+package br.com.ifc.pagarme.controller;
+
+import br.com.ifc.pagarme.model.Resposta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pagarme")
+public class PagarMeController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Value("${message:Default}")
+    private String message;
+	
+	@GetMapping("/servico")
+	public Resposta parser(@RequestParam("msg") String msg) {		
+
+		Resposta resposta = new Resposta();
+		resposta.setPorta(Integer.parseInt(environment.getProperty("local.server.port")));
+		resposta.setMensagem(msg);
+		resposta.setMensagemSistema(message);
+		return resposta;
+	}
+
+}
---
+++ src/main/java/br/com/ifc/pagarme/model/Resposta.java
@@ -0,0 +1,33 @@
+package br.com.ifc.pagarme.model;
+
+public class Resposta {
+	private int porta;
+	
+	private String mensagem;
+	
+	private String mensagemSistema;
+
+	public String getMensagemSistema() {
+		return mensagemSistema;
+	}
+
+	public void setMensagemSistema(String mensagemSistema) {
+		this.mensagemSistema = mensagemSistema;
+	}
+
+	public int getPorta() {
+		return porta;
+	}
+
+	public void setPorta(int porta) {
+		this.porta = porta;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}	
+}

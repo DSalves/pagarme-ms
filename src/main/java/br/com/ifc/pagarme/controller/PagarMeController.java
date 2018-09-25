4e3742c95dca1a0823c0dcd24d4acd5f4fd1bc72
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-25 18:53:21 -0300

    --

--- src/main/java/br/com/ifc/pagarme/controller/PagarMeController.java
+++ src/main/java/br/com/ifc/pagarme/controller/PagarMeController.java
@@ -4,6 +4,8 @@ import br.com.ifc.pagarme.model.Resposta;
 
 import java.net.InetAddress;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -14,10 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
 @RefreshScope
 @RequestMapping("/pagarme")
 public class PagarMeController {
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(PagarMeController.class);
 
 	@Autowired
 	private Environment environment;
@@ -38,12 +44,14 @@ public class PagarMeController {
 				// TODO: handle exception
 			}
 		}
-		
+				
 		Resposta resposta = new Resposta();
 		resposta.setPorta(Integer.parseInt(environment.getProperty("local.server.port")));
 		resposta.setIp(ip);
 		resposta.setMensagem(msg);
 		resposta.setMensagemSistema(message);
+		
+		LOGGER.info("Request of PagarME Service with answer " + new Gson().toJson(resposta) + "!");
 		return resposta;
 	}
 

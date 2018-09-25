0f622c7a6f19a593217bf80937983cbf1c0acef4
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-25 12:15:17 -0300

    --

--- src/main/java/br/com/ifc/pagarme/main/Application.java
+++ src/main/java/br/com/ifc/pagarme/main/Application.java
@@ -3,8 +3,10 @@ package br.com.ifc.pagarme.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages= {"br.com.ifc.pagarme.config", "br.com.ifc.pagarme.controller", "br.com.ifc.pagarme.filter"})
 public class Application {
 
